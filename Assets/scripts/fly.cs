8648c96631b9b1f9d030334d6092eb4765e46fea
odin <shency99@163.com>
2020-06-30 19:13:28 +0800

    choice fixed

--- Assets/scripts/fly.cs
+++ Assets/scripts/fly.cs
@@ -5,22 +5,6 @@ using UnityEngine;
 public class fly : MonoBehaviour
 {
 
-<<<<<<< HEAD
-   // Start is called before the first frame update
-    public float speed;
-    //private Rigidbody2D bird;
-    private GameObject[] array;
-    private Rigidbody2D[] rigid=new Rigidbody2D[7];
-    private bool run=false;
-    void Start()
-    {
-        array= GameObject.FindGameObjectsWithTag("Bird");
-        for(int i=0;i<7;++i)
-        {
-            rigid[i]=array[i].GetComponent<Rigidbody2D>();
-        }
-        
-=======
     // Start is called before the first frame update
     public float speed;
     public Animator anim;
@@ -37,8 +21,6 @@ public class fly : MonoBehaviour
             rigid[i] = array[i].GetComponent<Rigidbody2D>();
         }
 
->>>>>>> choice
-
     }
 
     // Update is called once per frame
@@ -48,24 +30,7 @@ public class fly : MonoBehaviour
     }
     void move()
     {
-<<<<<<< HEAD
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-           
-           run=true;
-            //transform.Translate()
-        } 
-        if(run)
-        {
-            Debug.Log("按F");
-            for(int i=0;i<7;++i)
-            {
-                Debug.Log(i+" "+array[i].transform.position.y);
-                if(array[i].transform.position.y>=i-5)
-                {
-                    Debug.Log("循环");
-                    rigid[i].velocity=Vector2.zero;
-=======
+
         if (Choice.choice == 0)
         {
 
@@ -82,21 +47,11 @@ public class fly : MonoBehaviour
                 {
                     // Debug.Log("循环");
                     rigid[i].velocity = Vector2.zero;
->>>>>>> choice
                     Debug.Log(rigid[i].velocity.y);
                 }
                 else
                 {
                     //Debug.Log("循环");
-<<<<<<< HEAD
-                    rigid[i].velocity=new Vector2(rigid[i].velocity.x,speed);
-                    Debug.Log(rigid[i].velocity.y);
-                }
-                
-            }         
-        }
-       
-=======
                     rigid[i].velocity = new Vector2(rigid[i].velocity.x, speed);
                     Debug.Log(rigid[i].velocity.y);
                 }
@@ -104,6 +59,5 @@ public class fly : MonoBehaviour
             }
         }
 
->>>>>>> choice
     }
 }
