e0d218a6b37b8c2192c6d41ac2e2083b6382cce5
Paitesanshi <45314239+Paitesanshi@users.noreply.github.com>
2020-06-30 23:41:32 +0800

    修鸟

---
+++ Assets/scripts/fly2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fly2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    private bool run = false;
+    private Rigidbody2D bird;
+    void Start()
+    {
+       
+        bird=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+    void move()
+    {
+        if (Choice.choice == 0)
+        {
+
+            run = true;
+            //transform.Translate()
+        }
+        if (run)
+        {
+            bird.velocity = new Vector2(bird.velocity.x, speed);
+            Debug.Log(transform.position.y );
+            if (transform.position.y >=-4)
+            {
+                bird.velocity = Vector2.zero;
+            }
+             
+            
+        }
+
+    }
+}
